d56a23088e9b21345079fd33b4c943ca24efc302
Aluno <ShadowDragonRub@gmail.com>
2022-10-17 10:19:40 -0300

    Initial commit

--- model.py
+++ model.py
@@ -2,5 +2,10 @@ from main import database as db
 
 
 class Character(db.Model):
-
-    id = db.Column()
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    created = db.Column(db.DataTime, nullable=False)
+    gender = db.Column(db.String(5), nullable=True)
+    status = db.Column(db.String(5), nullable=True)
+    species = db.Column(db.String(20), nullable=True)
+    type = db.Column(db.String(10), nullable=True)
