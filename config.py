ab44f7f0aa03b15d20c4aef3c42f3b85e4a9bad4
Aluno <ShadowDragonRub@gmail.com>
2022-10-17 09:48:09 -0300

    Initial commit

--- config.py
+++ config.py
@@ -3,12 +3,17 @@ import os
 
 class Config(object):
     TESTING = False
+    DEBUG = False
 
 
 class ProductionConfig(Config):
     DATABASE_URI = os.getenv("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
 
 
 class DevelopmentConfig(Config):
-    DATABASE_URI = "sql:///:memory:"
-    TESTING = True
+    TESTING = DEBUG = True
+
+    DATABASE_URI = "sqlite:///:memory:"
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
---
+++ model.py
@@ -0,0 +1,6 @@
+from main import database as db
+
+
+class Character(db.Model):
+
+    id = db.Column()
